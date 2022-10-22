c6f464794760cb94baf81d2c27400c7ae31e0997
amine <aminemalyana12@gmail.com>
2022-10-22 18:00:58 +0100

    validation

--- starter/assets/js/main.js
+++ starter/assets/js/main.js
@@ -5,6 +5,27 @@
  let x=18;
  let tempid;
  reloadTasks() ;
+ 
+ 
+ let save = document.getElementById("save1");
+ let title_error = document.getElementById("title_error");
+ save.disabled=true;
+ //=====================
+ save.addEventListener("keyup",function(){
+     if ( title.value !=" "){
+         save.disabled=false;
+     }else{
+         title_error.innerHTML = "hcdjhdbc";
+         save.disabled= true;
+         console.log("error tittle")
+     }
+
+ })
+
+
+
+
+
 
  function createTask() {
     // initialiser task form
@@ -26,6 +47,8 @@ function saveTask() {
     let s_value = status.options[status.selectedIndex].text;
     let date = document.getElementById("date");
     let description = document.getElementById("description");
+    
+    //=====================
 
     var checked;
     if(type_bug.checked==true){
@@ -153,14 +176,22 @@ function reloadTasks() {
     let todo = document.getElementById("to-do-tasks");
     let progress = document.getElementById("in-progress-tasks");
     let done = document.getElementById("done-tasks");
+    let tdindex = document.getElementById("to-do-tasks-count");
+    let ipindex = document.getElementById("in-progress-tasks-count");
+    let dnindex = document.getElementById("done-tasks-count");
+
 
     todo.innerHTML="";
     progress.innerHTML="";
     done.innerHTML="";
     // Set Task count
-
+            let todoCount=0;
+            let ipCount=0;
+            let doneCount=0;
     for(let i=0 ; i< tasks.length ;i++){
             if(tasks[i].status=='To Do'){
+                todoCount++;
+                tdindex.innerText=todoCount;
 
                 todo.innerHTML+= `
                                     <button class="py-2 d-flex align-items-center gap-4 px-3 rounded border-1"  onclick="updateTask(${tasks[i].taskid})" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">
@@ -183,6 +214,8 @@ function reloadTasks() {
                             // todo.innerHTML+=button1;
             }
             else if(tasks[i].status=='In Progress'){
+                ipCount++;
+                ipindex.innerText=ipCount;
 
                 let button1 = `
                                 <button class="py-2 d-flex align-items-center gap-4 px-3 rounded border-1" id="${tasks[i].taskid}" onclick="updateTask(${tasks[i].taskid})" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">
@@ -205,6 +238,10 @@ function reloadTasks() {
                         progress.innerHTML+=button1;
         }
         else if(tasks[i].status=='Done'){
+
+            doneCount++;
+            dnindex.innerText=doneCount;
+            
 
             let button1 = `
                             <button class="py-2 d-flex align-items-center gap-4 px-3 rounded border-1" id="${tasks[i].taskid}" onclick="updateTask(${tasks[i].taskid})" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">
@@ -231,3 +268,37 @@ function reloadTasks() {
     }
     }
 }
+
+function validate() {
+
+    let title = document.getElementById("title1");
+    let type_bug = document.getElementById("bug");
+    let type_feature = document.getElementById("feature");
+    let priority = document.getElementById("form_options_priority");
+    let p_value = priority.options[priority.selectedIndex].text;
+    let status = document.getElementById("form_options_status");
+    let s_value = status.options[status.selectedIndex].text;
+    let date = document.getElementById("date");
+    let description = document.getElementById("description");
+    let save = document.getElementById("save1");
+    let title_error = document.querySelector(".title_error");
+
+    save.disablad=true;
+
+    save.addEventListener("click", function(){
+        validate_title();
+    })
+
+}
+    function validate_title() {
+
+        
+
+
+    }
+
+
+
+
+
+
